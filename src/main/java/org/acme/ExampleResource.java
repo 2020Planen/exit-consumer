151658095fa0d15d77822d982a22177962691174
Mathias <mathias.b.k@hotmail.com>
2020-02-04 14:36:51 +0100

    first

---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,30 @@
+package org.acme;
+import io.smallrye.reactive.messaging.annotations.Channel;
+import org.reactivestreams.Publisher;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.SseElementType;
+
+/**
+ * A simple resource retrieving the in-memory "my-data-stream" and sending the items as server-sent events.
+ */
+@Path("/consumer")
+public class ExampleResource {
+
+    @Inject
+    @Channel("exit") Publisher<String> elements; 
+
+    @GET
+    @Path("/stream")
+    @Produces(MediaType.SERVER_SENT_EVENTS) 
+    @SseElementType("application/json") 
+    public Publisher<String> stream() { 
+        System.out.println("Received ---------------------------");
+        
+        return elements;
+    }
+}
